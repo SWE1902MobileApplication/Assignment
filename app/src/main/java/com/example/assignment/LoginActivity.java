63951961c5f200fb7ecb9b04d7899f0fabd9fc98
lishien1210 <kohlishien1210@gmail.com>
2021-11-10 10:19:33 +0800

    Add Basic UI

---
+++ app/src/main/java/com/example/assignment/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;import com.example.gogreengo.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/assignment/UserLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;import com.example.gogreengo.R;
+
+public class UserLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+}

943f0aec47c2bfc92b6ea662f08f6a763843a286
lishien1210 <kohlishien1210@gmail.com>
2021-11-30 01:17:36 +0800

    Add label for recycle item Add weight for each recycle item

--- app/src/main/java/com/example/assignment/EventEditActivity.java
+++ app/src/main/java/com/example/assignment/EventEditActivity.java
@@ -4,23 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
-import com.example.assignment.CalendarUtils;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 
 public class EventEditActivity extends AppCompatActivity
 {
     private EditText eventNameET;
     private TextView eventDateTV;
-    CalendarUtils cu = ((Global)getApplication()).getUtil();
+    CalendarUtils cu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        cu = ((Global)getApplication()).getUtil();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_edit);
         initWidgets();
-        eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
+        eventDateTV.setText("Date: " + cu.formattedDate(cu.selectedDate));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, LABEL );
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.label);
+        textView.setAdapter(adapter);
+
+        ElegantNumberButton button = (ElegantNumberButton) findViewById(R.id.number_button);
+        button.setOnClickListener(new ElegantNumberButton.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String num = button.getNumber();
+            }
+        });
+
     }
+
+    private static final String[] LABEL = new String[] {
+            "Paper", "Plastic bag", "Newspaper", "Box", "Iron", "Glasses"
+    };
 
     private void initWidgets()
     {
@@ -31,8 +55,9 @@ public class EventEditActivity extends AppCompatActivity
     public void saveEventAction(View view)
     {
         String eventName = eventNameET.getText().toString();
-        Event newEvent = new Event(eventName, CalendarUtils.selectedDate);
+        Event newEvent = new Event(eventName, cu.selectedDate);
         Event.eventsList.add(newEvent);
         finish();
     }
+
 }
